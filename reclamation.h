d934f6a5c82b82d4a3bddf38725207c767c63dff
Sellinihadil <hadil.sellini@esprit.tn>
2022-11-11 00:17:10 -0800

    ajouter fichier reclamation.h

--- reclamation.h
+++ reclamation.h
@@ -1,6 +1,28 @@
+#ifndef RECLAMATION_H_INCLUDED
+#define RECLAMATION_H_INCLUDED 
+#include <stdio.h>
+
+typedef struct
+{
+int jour;
+int mois;
+int annee;
+} Date;
+
+
 typedef struct{
-    char rec_txt[500];
-    int rec_type;
-    int list_rec;
-    int bur_rec;
-} Reclamation;
+int Id;
+Date daterec;
+int Liste_elect_rec;
+int num_bv;
+char desc_rec [1000];
+int type_rec;
+} reclamation;
+
+int ajouterrec(char * filename,reclamation c);
+int modifierrec(char * filename, int Id,reclamation nouv);
+int supprimerrec(char * filename,int Id);
+int chercherrec(char * filename,int Id);
+
+#endif 
+   
