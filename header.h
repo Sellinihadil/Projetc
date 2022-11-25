fc9108cf8bcc8017dbead890ea11373a210c4b6e
moniajebabli <115783931+moniajebabli@users.noreply.github.com>
2022-11-25 07:51:02 +0100

    stat taux obs et electeurs

--- header.h
+++ header.h
@@ -1,22 +1,36 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
-typedef struct{
-int jour;
-int mois;
-int annee;
+
+typedef struct {
+  char jour[3];
+  char mois[3];
+  char annee[5];
 }date;
-typedef struct{
-char municipalite[30];
-int Idbureau;
-char adresse[30];
-int numsalle;
-int Idagent;
-int capaciteelecteurs;
-int capaciteobservateurs;
- date date_ajout;
-}bureau;
-int ajouterbureau(bureau b,char *filename);
-int modifierbureau( int Idbureau,bureau nouv,char *filename);
-int supprimerbureau( int Idbureau,char *filename);
-bureau chercher(int Idbureau,char *filename);
+typedef struct {
+  char id[8];
+  char cin[8];
+  char username[100];
+  char password[100];
+  char nom[100];
+  char prenom[100];
+  date d;
+  char sexe[2];
+  char num_bureau[4];
+  char vote[2];
+}electeur;
+
+  typedef struct {
+  char nationalite[30];
+  char id[30];
+  char username[30];
+  char password[30];
+  char nom[30];
+  char prenom[30];
+  char appartenance[30];
+  char sexe[2];
+  char num_bureau[4];
+}observateur;
+void taux ( char *filename , float *tn, float *te);
+int nbe(char *filename,int id);
+
 #endif // HEADER_H_INCLUDED
--- main.c
+++ main.c
@@ -2,28 +2,16 @@
 #include <stdlib.h>
 #include "header.h"
 int main()
-{
-    bureau b1={"Ariana",234,"Menzah",2,654,13,5};
-    bureau b2={"Tunis",454,"Cite",3,987,16,7};
-    bureau b3;
-    int x;
-    x=ajouterbureau(b1,"bureau.txt");
-    if (x==1)
-        printf("\n ajout d'un bureau avec succees!!");
-    else printf("\n echec d'ajout");
-    x=modifierbureau( 234,b2,"bureau.txt");
-    if(x==1)
-        printf("\nModifie avec succes");
-        else printf("\n echec modification");
-        x=supprimerbureau("bureau.txt",234);
-        if(x==1)
-        printf("\n suppression avec succes");
-        else printf("\n echec de suppression");
-   
-b1=chercher(234,"bureau.txt");
-if (b1.Idbureau==-1)
-printf("bureau inexistant");
-else
-printf("%s%d%s%d%d%d%d\n",b1.municipalite,b1.Idbureau,b1.adresse,b1.numsalle,b1.Idagent,b1.capaciteelecteurs,b1.capaciteobservateurs);
-  return 0;
+{ float pourcent;
+ float tn,te;
+int k;
+observateur o1={"tunisien",345,67,"kkk","slim","kefi","isi","f",4};
+taux("observateur.txt",&tn,&te);
+printf ("le taux d'observateurs etrangers est egale a=%f\n",te);
+printf ("le taux d'observateurs tunisiens est egale a=%f\n",tn);
+electeur e1={445,765,65,"KIH","lilia","chebbi","samedi","mai",2010,"f",6};
+electeur e2={45,75,5,"KIl","nour","ali","samedi","juillet",2000,"f",3};
+k=nbe("electeur.txt",445);
+ printf("le nombre d'electeurs par bureau de vote=%d\n",k);
+    return 0;
 }
